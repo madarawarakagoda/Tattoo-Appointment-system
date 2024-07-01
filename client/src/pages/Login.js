8eb38e49e548f9ed2a3adbb3d061eca93ee6eb66
madarawarakagoda <madarawarakagoda8@gmail.com>
2024-07-01 10:09:37 +0530

    create some pages

---
+++ client/src/pages/Login.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "../styles/RegisterStyles.css";
+import { Form, Input } from "antd";
+import { Link} from "react-router-dom";
+
+
+
+const Login = () => {
+  
+  //form handler
+  const onfinishHandler = async (values) => {
+    console.log(values)
+    
+  };
+  return (
+    <div className="form-container ">
+      <Form
+        layout="vertical"
+        onFinish={onfinishHandler}
+        className="register-form"
+      >
+        <h3 className="text-center">Login From</h3>
+
+        <Form.Item label="Email" name="email">
+          <Input type="email" required />
+        </Form.Item>
+        <Form.Item label="Password" name="password">
+          <Input type="password" required />
+        </Form.Item>
+        <Link to="/register" className="m-2">
+          Not a user Register here
+        </Link>
+        <button className="btn btn-primary" type="submit">
+          Login
+        </button>
+      </Form>
+    </div>
+  );
+};
+
+export default Login;
+
+
+
+
+
+
+
+
+
+
