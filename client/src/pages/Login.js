ab9da9682b7a4a7b4b801a6d974b73ea753b8431
madarawarakagoda <madarawarakagoda8@gmail.com>
2024-07-02 22:39:06 +0530

    update home page,usecrtl

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,16 +1,30 @@
 import React from "react";
 import "../styles/RegisterStyles.css";
-import { Form, Input } from "antd";
-import { Link} from "react-router-dom";
+import { Form, Input,message } from "antd";
+import { Link,useNavigate} from "react-router-dom";
+import axios from 'axios'
 
 
 
 const Login = () => {
-  
+  const navigate = useNavigate();
+
   //form handler
   const onfinishHandler = async (values) => {
-    console.log(values)
-    
+    try {
+
+      const res = await axios.post("/api/v1/user/login", values);
+      if (res.data.success) {
+        localStorage.setItem("token", res.data.token);
+        message.success("Login Successfully");
+        navigate("/");
+      } else {
+        message.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      message.error("something went wrong");
+    }
   };
   return (
     <div className="form-container ">
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,8 +2,9 @@ const express = require("express");
 const {
   loginController,
   registerController,
+  authController,
 } = require("../controllers/userCtrl");
-
+const authMiddleware = require("../middlewares/authMiddleware");
 
 //router onject
 const router = express.Router();
@@ -15,5 +16,6 @@ router.post("/login", loginController);
 //REGISTER || POST
 router.post("/register", registerController);
 
-
+//Auth || POST
+router.post('/getUserData',authMiddleware,authController);
 module.exports = router;
