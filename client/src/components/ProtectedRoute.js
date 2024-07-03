80153a43e179ef8b7ff986a08a778ef81bdda021
madarawarakagoda <madarawarakagoda8@gmail.com>
2024-07-03 21:15:14 +0530

    create protectedroutes

---
+++ client/src/components/ProtectedRoute.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+export default function ProtectedRoute({Children}) 
+{
+ if(localStorage.getItem("token")){
+    return Children;
+ } else{
+    return <Navigate to="login"/>;
+ }
+
+}
