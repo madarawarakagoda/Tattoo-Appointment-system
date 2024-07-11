d3457d9ac97f20afa604e52772a1c5234103e4b9
madarawarakagoda <madarawarakagoda8@gmail.com>
2024-07-11 10:58:50 +0530

    delete some

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,72 +1,42 @@
-import React from "react";
-import '../styles/LayoutStyles.css';
-import { adminMenu,userMenu } from "../Data/data";
+import React, {useEffect } from "react";
 import axios from "axios";
-import { useEffect } from "react";
-import { Layout } from "antd";
-import { Link, useLocation } from 'react-router-dom';
-import { useSelector } from "react-redux";
+import '../styles/LayoutStyles.css';
+
 
 const HomePage = () => {
-    // login usr data
+    //login usr data
     const getUserData = async () => {
-        try {
-            const res = await axios.post(
-                "/api/v1/user/getUserData",
-                {},
-                {
-                    headers: {
-                        Authorization: "Bearer " + localStorage.getItem("token"),
-                    },
-                }
-            );
-        } catch (error) {
-            console.log(error);
-        }
-    };
+      try{
+       const res = await axios.post(
+      "/api/v1/user/getUserData",
+      {},
+      {
+        headers:{
+          Authorization: "Bearer "+localStorage.getItem("token"),
 
-    useEffect(() => {
-        getUserData();
-    }, []);
-
-    const { user } = useSelector(state => state.user);
-    const location = useLocation();
-    //redering menu List
-    const SidebarMenu = user?.isAdmin ? adminMenu : userMenu;
-    return (
-        <Layout>
-            <div className='main'>
-                <div className='layout'>
-                    <div className='sidebar'>
-                        <div className='logo'>
-                            <h6>SKIN CRAFT</h6>
-                            <hr />
-                        </div>
-                        <div className='menu'>
-                            {SidebarMenu.map((menu) => {
-                                const isActive = location.pathname === menu.path;
-                                return (
-                                    <div className={`menu-item ${isActive && "active"}`} key={menu.path}>
-                                        <i className={menu.icon}></i>
-                                        <Link to={menu.path}>{menu.name}</Link>
-                                    </div>
-                                );
-                            })}
-                        </div>
-                    </div>
-                    <div className='content'>
-                        <div className='header'>
-                            <div className="header-content">
-                                <i className="fa-solid fa-bell"></i>
-                               <Link to="/profile">{user?.name}</Link>
-                            </div>
-                        </div>
-                        <div className='body'></div>
-                    </div>
-                </div>
-            </div>
-        </Layout>
+        },
+      }
     );
+  }catch(error){
+    console.log(error);
+  }
+};
+
+useEffect(()=>{
+  getUserData();
+},[]);
+
+
+
+return (
+<div>
+    <h1>Home page</h1>
+</div>
+
+
+      
+     
+  );
 };
 
 export default HomePage;
